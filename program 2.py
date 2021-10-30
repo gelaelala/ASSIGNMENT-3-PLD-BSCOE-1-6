fee4c63f82a79d15205712585504f3d6e24fde52
unknown <angelaescariocorpuz@gmail.com>
2021-10-31 00:41:30 +0800

    Second program under assignment number 3

--- program 2.py
+++ program 2.py
@@ -6,21 +6,3 @@ def amntoforanges ():
     Oranges = int(input("Enter the amount of oranges: "))
     return Oranges
 
-def totalapples ():
-    total_apples = int(amntofapples*20)
-    return total_apples
-
-def totaloranges ():
-    total_oranges = int(amntoforanges*25)
-    return total_oranges
-
-def totalprice ():
-    total_price = int(totalapples+totaloranges)
-    return total_price
-
-def display (Total):
-    print (f'The total amount is {Total}')
-
-Apples = amntofapples ()
-Oranges = amntoforanges ()
-display ()
