8ed994b90d792495aef831c27dcb852a9525bb6e
unknown <angelaescariocorpuz@gmail.com>
2021-10-31 01:13:26 +0800

    Third program under assignment number 3

---
+++ program 3.py
@@ -0,0 +1,10 @@
+def money_ ():
+    money = int(input('Enter the amount of money: '))
+    return money
+def price_apple ():
+    price = int(input('Enter the price of the apple: '))
+    return price
+
+
+Money = money_ ()
+Price = price_apple ()
