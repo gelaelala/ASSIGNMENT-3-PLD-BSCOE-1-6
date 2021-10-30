b0d7ea709545f83e12ece422df9fa07bfa0f38b3
unknown <angelaescariocorpuz@gmail.com>
2021-10-31 00:13:54 +0800

    Second program under assignment number 3

---
+++ program 2.py
@@ -0,0 +1,6 @@
+def amntofapples ():
+    Apples = int(input("Enter the amount of apples: "))
+    return Apples
+def amntoforanges ():
+    Oranges = int(input("Enter the amount of oranges: "))
+    return Oranges
